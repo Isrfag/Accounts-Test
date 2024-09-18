1ed82fe2398c8c6b3d2143a8a2b2a224194f3c49
unknown <isrfag@gmail.com>
2024-09-18 11:33:09 +0200

    Subiendo reformulacion de test

--- src/test/java/com/microcompany/accountsservice/persistence/AccountRepositoryTest.java
+++ src/test/java/com/microcompany/accountsservice/persistence/AccountRepositoryTest.java
@@ -30,13 +30,13 @@ class AccountRepositoryTest {
 
     @Test
     @Order(1)
-    void createAccounts_WhenAccountIsValid(){
+    void givenAnAccount_WhenCreateAccounts_ThenAccountIsValid(){
         Account account = new Account(null, "Personal", LocalDate.now(), 100, 1L);
         assertThat(repository.save(account)).isNotNull();
     }
 
     @Test
-    void createAccounts_WhenAccountIsNotValid(){
+    void givenAnInvalidadAccount_WhenAccountIsNotValid_ThenReturnException(){
         Account account = null;
         assertThrows(Exception.class,() ->
             repository.save(account)
@@ -44,44 +44,44 @@ class AccountRepositoryTest {
     }
 
     @Test
-    void givenAccount_WhenAidIsNotNull(){
+    void givenAnAccount_WhenAidIsNotNull_ThenReturnAccount(){
         assertThat(repository.findById(1L).get()).isNotNull();
     }
 
     @Test
-    void givenAccount_WhenAidIsNull(){
+    void givenAnInvalidAccount_WhenAidIsNull_ThenReturnException(){
         assertThrows(Exception.class,() ->
                 repository.findById(100L).get()
         );
     }
 
     @Test
-    void givenAccountsByOwnerId_WhenAidIsNotNull(){
+    void givenAccount_WhenOwnerIdIsGood_ThenReturnAccount(){
         List<Account> accounts = repository.findByOwnerId(1L);
         assertThat(accounts).isNotNull().isNotEmpty();
     }
 
     @Test
-    void givenAccountsByOwnerId_WhenAidIsNull(){
+    void givenAccounts_WhenOwnerIdIsInvalid_ThenReturnNull(){
         List<Account> accounts = null;
         assertThat(accounts).isNull();
     }
 
     @Test
-    void givenAccountsByOwnerId_WhenAidIsEmpty(){
+    void givenAccountsByOwnerId_WhenOwnerIdIsInvalidAndHaveNoAccounts_ThenReturnAnEmptyList(){
         List<Account> accounts = repository.findByOwnerId(5L);
         assertThat(accounts).isEmpty();
     }
 
     @Test
-    void updateAccountByOwnerId_WhenAccountIsNotNull(){
+    void givenAnAccountToUpdate_WhenOwnerIdIsValid_ThenReturnUpdatedAccount(){
         Account account = repository.findById(1L).get();
         account.setType("Company");
         assertThat(repository.save(account)).isNotNull();
     }
 
     @Test
-    void updateAccountByOwnerId_WhenAccountIsNull(){
+    void givenAnAccountToUpdate_WhenAccountCannotBeUpadted_ThenReturnNull(){
         Account account = null;
         assertThrows(Exception.class,() ->
             repository.save(account)
@@ -89,14 +89,14 @@ class AccountRepositoryTest {
     }
 
     @Test
-    void updateAccountByOwnerId_WhenAccountToUpdateDoesntExist(){
+    void givenAnAccountToUpdateByOwnerId_WhenAccountToUpdateDoesntExist_ThenReturnException(){
         assertThrows(Exception.class,() ->
            repository.findById(40L).get()
         );
     }
 
     @Test
-    void updateAccountByOwnerId_WhenCustomerIsNull(){
+    void givenAnAccountUpdateAccountByOwnerId_WhenCustomerIsNull_ThenReturnException(){
         Long customerId = null;
         assertThrows(Exception.class,() ->
             customerRepository.findById(customerId)
@@ -104,21 +104,21 @@ class AccountRepositoryTest {
     }
 
     @Test
-    void removeAccountByOwnerId_WhenAccountIsNotNull(){
+    void givenAnAccountToRemoveByOwnerId_WhenAccountIsNotNull_ThenReturnFalse(){
         assertTrue(repository.findById(1L).isPresent());
         repository.deleteById(1L);
         assertFalse(repository.findById(1L).isPresent());
     }
 
     @Test
-    void removeAccountsByOwnerId_WhenAccountIsNotNull(){
+    void givenAnAccountToRemoveOwnerId_WhenAccountIsNotNull_ThenReturnLessThan1(){
         assertThat(repository.findByOwnerId(1L).size()).isGreaterThan(0);
         repository.deleteByOwnerId(1L);
         assertThat(repository.findByOwnerId(1L).size()).isLessThan(1);
     }
 
     @Test
-    void removeAccountByOwnerId_WhenAccountIsNull(){
+    void givenAnAccountToRemoveByOwnerId_WhenAccountIsNull_ThenReturnException(){
         Long account = null;
         assertThrows(Exception.class,() ->
             repository.findById(account)
@@ -126,7 +126,7 @@ class AccountRepositoryTest {
     }
 
     @Test
-    void removeAccountsByOwnerId_WhenCustomerIsNull(){
+    void givenAnAccountToRemoveAccountsByOwnerId_WhenCustomerIsNull_ThenReturnException(){
         assertThrows(Exception.class,() ->
            customerRepository.findById(40L).get()
         );
